2479523857354db219a6d1c3024aad98cba90cee
unknown <bouchra.benkehil@ensia.edu.dz>
2023-04-29 09:54:29 -1100

    commit

---
+++ themes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Website</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<a href="#">My Website</a>
+			</div>
+
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
---
+++ themes/sidebar.php
@@ -0,0 +1,12 @@
+<aside>
+	<div class="container">
+		<h3>Recent Posts</h3>
+		<ul>
+			<li><a href="#">Post 1</a></li>
+			<li><a href="#">Post 2</a></li>
+			<li><a href="#">Post 3</a></li>
+			<li><a href="#">Post 4</a></li>
+			<li><a href="#">Post 5</a></li>
+		</ul>
+	</div>
+</aside>
